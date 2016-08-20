0c577d2dbf84c4fd9e0c53908a248bd22351780b
Dinh Phong <dinhphong.developer@gmail.com>
2016-08-20 14:20:00 +0700

    Refactor Home Controller & Route Class

--- core/Route.php
+++ core/Route.php
@@ -13,60 +13,60 @@ namespace Rainy;
 
 class Route
 {
-	protected static $listRoute = [];
+    protected static $listRoute = [];
 
-	public static function rewrite($urlRequest, $callback)
-	{
-		$route = array();
+    public static function rewrite($urlRequest, $callback)
+    {
+        $route = array();
 
-		$urlRequest = filter_var(trim($urlRequest, '/'), FILTER_SANITIZE_URL);
+        $urlRequest = filter_var(trim($urlRequest, '/'), FILTER_SANITIZE_URL);
 
-		$route['regex'] = self::buildUrl($urlRequest);
+        $route['regex'] = self::buildUrl($urlRequest);
 
-		if(is_string($callback)) {
-			$callback = explode('@',$callback);
-			$controller = $callback[0];
-			$method = $callback[1];
+        if(is_string($callback)) {
+            $callback = explode('@',$callback);
+            $controller = $callback[0];
+            $method = $callback[1];
 
-			$route['controller'] = $callback[0];
-			$route['method'] = $callback[1];
-		} else {
-			$route['callback'] = $callback;
-		}
+            $route['controller'] = $callback[0];
+            $route['method'] = $callback[1];
+        } else {
+            $route['callback'] = $callback;
+        }
 
-		return self::$listRoute[$urlRequest] = $route;
-	}
+        return self::$listRoute[$urlRequest] = $route;
+    }
 
-	public static function show()
-	{
-		echo "<pre>";
-		var_dump(self::$listRoute);
-		echo "</pre>";
-	}
+    public static function show()
+    {
+        echo "<pre>";
+        var_dump(self::$listRoute);
+        echo "</pre>";
+    }
 
-	public static function getAllRoutes()
-	{
-		return self::$listRoute;
-	}
+    public static function getAllRoutes()
+    {
+        return self::$listRoute;
+    }
 
-	public static function buildUrl($urlRequest)
-	{
+    public static function buildUrl($urlRequest)
+    {
 
-		$urlExplode = explode('/', $urlRequest);
-		$regex = '';
-		$params = array();
+        $urlExplode = explode('/', $urlRequest);
+        $regex = '';
+        $params = array();
 
-		foreach($urlExplode as $url) {
+        foreach($urlExplode as $url) {
 
-			if(strpos($url, "{") === 0 && substr($url, -1) == "}") {
-				$regex .= "([^\/]*?)\/";
-				continue;
-			}
+            if(strpos($url, "{") === 0 && substr($url, -1) == "}") {
+                $regex .= "([^\/]*?)\/";
+                continue;
+            }
 
-			$regex .= $url."\/";
-		}
+            $regex .= $url."\/";
+        }
 
-		return $regex = '/^' . trim($regex, '\/') . '$/';
+        return $regex = '/^' . trim($regex, '\/') . '$/';
 
-	}
+    }
 }
